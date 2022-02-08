685d627ed739427d7ec3db5d1b1cd624f2223b79
Jestin Issac James <jestin.i.james@gmail.com>
2022-02-08 13:06:16 +0530

    Second Commit.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('hi');
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`);
+});
