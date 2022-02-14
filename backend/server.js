04c7a6ae976fba7985025794128817e5482f8eb0
Jestin Issac James <jestin.i.james@gmail.com>
2022-02-14 19:38:56 +0530

    Prepare for Deploy.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
+const path = require('path');
 
 
 // Start express
@@ -22,9 +23,30 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tasks', require('./routes/taskRoutes'));
+
+// Serve Frontend
+if(process.env.NODE_ENV === 'production')
+{
+    // Set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    // Any routes we have not defined.
+    app.get('*', (req,res) => {
+        res.sendFile(__dirname, '../frontend', 'build', 'index.html');
+
+    }); 
+}
+
+else {
+    
+app.get('/', (req,res) => {
+    res.status(200).json({message: 'Welcome To My Task Master'});
+    });
+}
 
 
 app.listen(PORT, () => {
