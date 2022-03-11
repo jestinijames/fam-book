0ec34d09c7e6891c6776416d1611188a00bceafe
Jestin Issac James <jestin.i.james@gmail.com>
2022-03-11 13:47:58 +0530

    Tenth Commit - Integrated Socket.

--- frontend/src/pages/messenger/Messenger.jsx
+++ frontend/src/pages/messenger/Messenger.jsx
@@ -24,6 +24,33 @@ const Messenger = () => {
     const { user } = useSelector((state) => state.authReducer);
     const scrollRef = useRef();
 
+    useEffect(() => {
+        socket.current = io(process.env.REACT_APP_ENDPOINT);
+        socket.current.on("getMessage", (data) => {
+          setArrivalMessage({
+            sender: data.senderId,
+            text: data.text,
+            createdAt: Date.now(),
+          });
+        });
+      }, []);
+    
+      useEffect(() => {
+        arrivalMessage &&
+          currentChat?.members.includes(arrivalMessage.sender) &&
+          setMessages((prev) => [...prev, arrivalMessage]);
+      }, [arrivalMessage, currentChat]);
+    
+      useEffect(() => {
+        socket.current.emit("addUser", user._id);
+        socket.current.on("getUsers", (users) => {
+          setOnlineUsers(
+            user.followings.filter((f) => users.some((u) => u.userId === f))
+          );
+        });
+      }, [user]);
+    
+
 
     useEffect(() => {
         const getConversations = async () => {
